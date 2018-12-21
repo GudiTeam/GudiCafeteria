45c8f4138dfc04c6b71e1d8d9b8c8ba301e0f546
rlawlgns077 <rlawlgns077@gmail.com>
2018-12-21 17:48:38 +0900

    ...

--- GudiCafeteria_a/app/src/main/java/com/duzi/gudicafeteria_a/ui/review/ReviewAddActivity.kt
+++ GudiCafeteria_a/app/src/main/java/com/duzi/gudicafeteria_a/ui/review/ReviewAddActivity.kt
@@ -9,5 +9,7 @@ class ReviewAddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review_add)
+
+        // TODO ReviewAddViewModel - Repository -
     }
 }
