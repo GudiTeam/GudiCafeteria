07f0a2bc7fb791c7ef1e1b1fd48fa6a04b4fd8d7
rlawlgns077 <rlawlgns077@gmail.com>
2018-12-27 17:04:06 +0900

    리뷰 작성 액티비티 적용중..

--- GudiCafeteria_a/app/src/main/java/com/duzi/gudicafeteria_a/data/Comment.kt
+++ GudiCafeteria_a/app/src/main/java/com/duzi/gudicafeteria_a/data/Comment.kt
@@ -1,5 +1,9 @@
 package com.duzi.gudicafeteria_a.data
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Comment(
     val cafe_Id: String,
     val user_Id: String,
@@ -8,4 +12,4 @@ data class Comment(
     val comment_score: String,
     val ins_user_dtm: String,
     val upd_user_dtm: String
-)
+) : Parcelable
--- GudiCafeteria_a/app/src/main/java/com/duzi/gudicafeteria_a/ui/comment/CommentAddActivity.kt
+++ GudiCafeteria_a/app/src/main/java/com/duzi/gudicafeteria_a/ui/comment/CommentAddActivity.kt
@@ -1,15 +1,41 @@
 package com.duzi.gudicafeteria_a.ui.comment
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import com.duzi.gudicafeteria_a.R
+import com.duzi.gudicafeteria_a.base.BaseActivity
+import com.duzi.gudicafeteria_a.data.Comment
 
-class CommentAddActivity : AppCompatActivity() {
+class CommentAddActivity : BaseActivity() {
+
+    override val initView: () -> Unit = {}
+    override val requestedPermissionList: List<String> = listOf()
+    override val layoutResID: Int = R.layout.activity_review_add
+
+    private lateinit var commentViewModel: CommentViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_review_add)
 
-        // TODO ReviewAddViewModel - Repository -
+        val bundle = intent.getParcelableExtra<Comment>(COMMENT)
+        if(bundle != null) {
+
+        }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        commentViewModel = getViewModel()
+    }
+
+    companion object {
+        private const val COMMENT = "COMMENT"
+        fun open(context: Context, comment: Comment) {
+            val intent = Intent(context, CommentAddActivity::class.java)
+            intent.putExtra(COMMENT, comment)
+            context.startActivity(intent)
+        }
     }
 }
